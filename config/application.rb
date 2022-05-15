8dab353650836089205fd5116c8ff1b58d2a41af
alejandro2Lopez <alejandro@DESKTOP-RHK3T75.localdomain>
2022-05-14 20:08:26 -0600

    Clase lunes

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module Lenguajes1
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
+    I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{yml}')]
+    I18n.default_locale = :es
   end
 end
